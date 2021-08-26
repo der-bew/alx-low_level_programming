b2d82410c0939e30032f4630ac976ae874e0b897
der-bew <derbew12@gmail.com>
2021-08-26 17:03:40 +0300

    Update: 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-/** 
- *main - starting point
+/**
+ * main - starting point
  *
- *Return: Always return zero (Success) 
+ * Return: Always return 0 (Success)
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
