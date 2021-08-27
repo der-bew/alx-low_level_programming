a2fabb2ebda3e2d3345c73834d1968b2afe3eef2
der-bew <derbew12@gmail.com>
2021-08-27 18:44:43 +0300

    Add: 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Satrting point
+ *
+ *Return: zero
+ *
+ *Descripitions: Prints single digit number
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	printf("%d", num);
+	printf('\n');
+
+	return (0);
+}
