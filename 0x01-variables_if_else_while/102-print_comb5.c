e321c7980001c2be729f4b3cb6540668a97644dc
der-bew <derbew12@gmail.com>
2021-08-27 23:12:34 +0300

    Add: 102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 	{
 		for (num2 = 0; num2 <= 99; num2++)
 		{
-			if (((num1 == 0) && (num2 != 0)) || (num1 < num2))
+			if (num1 < num2)
 			{
 				putchar(num1 / 10 + '0');
 				putchar(num1 % 10 + '0');
