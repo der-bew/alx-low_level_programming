8dade66105399ac87ef563cac6df929b46d911f2
der-bew <derbew12@gmail.com>
2021-08-27 15:30:12 +0300

    Update: 0-positive_or_negative.c file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive", n);
+printf("%ld is positive", n);
 }
 else if (n < 0)
 {
-printf("%d is negative", n);
+printf("%ld is negative", n);
 }
 else
 {
-printf("%d is zero", n);
+printf("%ld is zero", n);
 }
 return (0);
 }
