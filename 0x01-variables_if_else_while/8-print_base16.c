deff5be667a01876e535e08c873069a8f20eee19
der-bew <derbew12@gmail.com>
2021-08-27 19:30:10 +0300

    Add: 8-print_base16.c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Satrting point
+ *
+ *Return: zero
+ *
+ *Descripitions: Prints Hexadecimal numbers in Lowercase
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 0; alp <= 9; alp++)
+	putchar(alp);
+	for (alp = 'a'; alp <= 'f'; alp++)
+	putchar(alp);
+	putchar('\n');
+
+	return (0);
+}
