872e749c58e0b0fd1773693afcc45ec1639c28ff
der-bew <derbew12@gmail.com>
2021-08-27 15:16:05 +0300

    Add: 0-positive_or_negative.c file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Starting point
+ *
+ *Return: 0
+ *
+ *Descripitions: To check a number is positive or negative
+ */
+
+int main(void)
+{
+if (n > 0)
+{
+printf("%d is positive", n);
+}
+else if (n < 0)
+{
+printf("%d is negative", n);
+}
+else
+{
+printf("%d is zero", n);
+}
+return (0);
+}
