4bc58dc5072165745abcbb29236a15d25610f608
der-bew <derbew12@gmail.com>
2021-08-27 18:12:32 +0300

    Add: 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Satrting point
+ *
+ *Return: zero
+ *
+ *Descripitions: Prints Alphabet in Lowercase
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	putchar(alp);
+	for (alp = 'A'; alp <= 'Z'; alp++)
+	putchar('\n');
+	putchar('\n');
+
+	return (0);
+}
