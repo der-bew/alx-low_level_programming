c5a1d054f32ab18918ebf24f57d5e3bf9d54907a
der-bew <derbew12@gmail.com>
2021-08-31 22:50:14 +0300

    Add: 101-natural.c c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - starting point
+ *
+ *Return: Aways (0)
+ */
+
+int main(void)
+{
+	int num, sum;
+	
+	num = 0;
+	sum = 0;
+	while(num < 1024)
+	{
+		if ((num % 5 == 0) || (num % 3 == 0))
+		{
+			sum += num;
+		}
+		num++;
+	}
+	printf("%d \n", sum);
+
+	return (0);
+}
