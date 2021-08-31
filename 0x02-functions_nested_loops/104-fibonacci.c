2659dc54496f2db39d5d67af15def3765554fedb
der-bew <derbew12@gmail.com>
2021-08-31 23:35:30 +0300

    Add: 104-fibonacci.c file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ *main - Finds and prints the first 98 Fibonacci numbers,
+ *starting with 1 and 2, followed by a new line.
+ *The numbers should be separated by comma, followed by a space ,
+ * You are allowed to use the standard library
+ *You are not allowed to use any other library (You can’t use GMP etc…)
+ *You are not allowed to use long long, malloc, pointers,
+ *arrays/tables, or structures
+ *You are not allowed to hard code any Fibonacci number (except for 1 and 2)
+ *
+ *Return: Always (0)
+ */
+
+int main(void)
+{
+	unsigned long int x, y, z, y1, y2, z1, z2;
+
+	y = 1;
+	z = 2;
+
+	printf("%lu", y);
+
+	for (x = 1; x < 91; x++)
+	{
+		printf(", %lu", z);
+		z += y;
+		y = z - y;
+	}
+
+	y1 = y / 1000000000;
+	y2 = y % 1000000000;
+	z1 = z / 1000000000;
+	z2 = z % 1000000000;
+
+	for (x = 92; x < 99; ++x)
+	{
+		printf(", %lu", z1 + (z2 / 1000000000));
+		printf("%lu", z2 % 1000000000);
+		z1 += y1;
+		y1 = z1 - y1;
+		z2 += y2;
+		y2 = z2 - y2;
+	}
+
+	printf("\n");
+
+	return (0);
+}
