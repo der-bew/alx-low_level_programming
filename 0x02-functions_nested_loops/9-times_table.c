2d76d5eb0380eb1c41da8b5c0b936fe70f983f48
der-bew <derbew12@gmail.com>
2021-08-31 19:45:03 +0300

    Update: 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,6 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			
 			if (result >  9)
 			{
 				_putchar(result / 10 + '0');
@@ -31,7 +30,7 @@ void times_table(void)
 				_putchar(j + '0');
 			}
 
-			if (j !=9)
+			if (j != 9)
 			{
 				_putchar(44);
 				_putchar(32);
@@ -40,4 +39,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-			
