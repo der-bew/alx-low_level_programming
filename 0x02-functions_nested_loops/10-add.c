4354cf8a7d4076b38f3ed820f246bc5314b09494
der-bew <derbew12@gmail.com>
2021-08-31 19:54:25 +0300

    Add: 10-add.c c file and Update: main.h header file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - Adds two integer number
+ *
+ *@num1: First integer number
+ *@num2: Second integer number
+ *
+ *Return: num1 + num2
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
