4d021df750c6868507ddea023d17212d27890ce1
der-bew <derbew12@gmail.com>
2021-09-01 15:54:44 +0300

    Add: 0-main.c c file, main.h header file, and README.md file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *main - test a function if a given number is zero
+ *Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
