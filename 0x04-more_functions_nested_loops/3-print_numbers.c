eb428d766083e0f451469697561af45e7d30d697
der-bew <derbew12@gmail.com>
2021-09-02 14:58:02 +0300

    Add: 3-print_numbers.c file and Update: main.h header file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_numbers - Prints numbers from 0 to 9
+ *Return: Nothing return
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 48;
+	while (n >= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
