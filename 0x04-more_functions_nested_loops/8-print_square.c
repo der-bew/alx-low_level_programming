25e6a32ac18e16a2e11e45a4bee6debe286a6753
der-bew <derbew12@gmail.com>
2021-09-02 17:26:20 +0300

    Add: 8-print_square.c file and Update main.h header file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_square - Prints a square
+ *@size: Input Integer
+ *Return: Nothing return
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
