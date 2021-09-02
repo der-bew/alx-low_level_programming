eecd4666479cbb8e2e2f18a08c7bc127c0111ab4
der-bew <derbew12@gmail.com>
2021-09-02 14:48:44 +0300

    Add: 2-mul.c file and Update: main.h header file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - It multiplies two integer
+ *@a: Integer one
+ *@b: Integer two
+ *Return: Multiplications of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
