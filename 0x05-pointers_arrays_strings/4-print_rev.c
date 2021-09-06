bd5e4d74d2187f0df6c1232a95e1ac738214bf09
der-bew <derbew12@gmail.com>
2021-09-06 19:35:17 +0300

    Add: for(i = length - 1; i >= 0; i--)

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_rev - Prints a string in reverse order
+ *@s: input string
+ *Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	for(i = length - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
