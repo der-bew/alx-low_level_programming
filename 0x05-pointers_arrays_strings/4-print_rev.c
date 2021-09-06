3204b9c0d4e079d95499dd50df6482bddb63d6a6
der-bew <derbew12@gmail.com>
2021-09-06 19:51:42 +0300

    Add: 4-print_rev.c file and Update the header file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,11 @@ void print_rev(char *s)
 {
 	int i, length = 0;
 
-	while (*s != '\0')
-	{
+	for (i = 0; *(s + i) != '\0'; i++)
 		length++;
-		s++;
 
 		for (i = length - 1; i >= 0; i--)
-		{
 			_putchar(*(s + i));
-		}
-	}
+
 	_putchar('\n');
 }
