a699f60e40addd6393c3ce9514d9fef774a2bd45
der-bew <derbew12@gmail.com>
2021-09-06 18:05:56 +0300

    Add: 3-puts.c file and Update the header file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - Prints a string
+ *@str: input string
+ *Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
