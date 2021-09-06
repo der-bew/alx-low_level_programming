53e8bdeb9bd335aca0fa485cba68d6fc58751237
der-bew <derbew12@gmail.com>
2021-09-06 17:22:37 +0300

    Add: 1-swap.c file and Update: main.h header file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *swap_int - Swaps the value of two integers
+ *@a: Integer one
+ *@b: Integer two
+ *Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
