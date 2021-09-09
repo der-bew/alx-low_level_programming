7042e31c7a4ac617ffb47fbf168b79bb50121e4e
der-bew <derbew12@gmail.com>
2021-09-09 17:09:02 +0300

    Add: 7-leet.c file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ **leet - Encodes a string into 1337
+ *@c: Input string
+ *Return: c
+ */
+
+char *leet(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] == 'a' || c[i] == 'A')
+		{
+			c[i] = '4';
+		}
+		else if (c[i] == 'e' || c[i] == 'E')
+		{
+			c[i] = '3';
+		}
+		else if (c[i] == 'o' || c[i] == 'O')
+		{
+			c[i] = '0';
+		}
+		else if (c[i] == 't' || c[i] == 'T')
+		{
+			c[i] = '7';
+		}
+		else if (c[i] == 'l' || c[i] == 'L')
+		{
+			c[i] = '1';
+		}
+	}
+
+	return (c);
+}
