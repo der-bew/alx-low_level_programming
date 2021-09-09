8e33534d8c82e840ee2f86f55004ded401b7b9c0
der-bew <derbew12@gmail.com>
2021-09-09 14:44:09 +0300

    Add: 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] > s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else if (s2[i] > s1[i])
+		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
