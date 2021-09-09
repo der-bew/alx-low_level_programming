5bceecb6b8f68a8617cd476491967e5a63faa071
der-bew <derbew12@gmail.com>
2021-09-09 18:41:48 +0300

    Update: 101-print_number.c file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,21 @@
 
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n < 0)
 	{
-		/*n = -n*/
+		num = -n;
 		_putchar('-');
-		n = -n;
+	}
+	else
+	{
+		num = n;
 	}
 
-	if (n > 9)
+	if (num / 10)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
