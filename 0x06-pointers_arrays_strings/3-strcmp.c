0ca98cf801aa9195847069bcac75b72682c14e6f
der-bew <derbew12@gmail.com>
2021-09-09 14:36:05 +0300

    Add: 3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_strcmp - Compares two string
+ *@s1: First string
+ *@s2: Second string
+ *Return: Integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] > s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else if (s2[i] > s1[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
