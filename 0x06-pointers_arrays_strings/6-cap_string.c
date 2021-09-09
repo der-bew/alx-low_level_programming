f979c77e9bf491958c549b1aa68c3cb940cd0d5b
der-bew <derbew12@gmail.com>
2021-09-09 16:31:56 +0300

    Add: 6-cap_string.c file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ **cap_string - Capitalizes all words of string
+ *@c: Input string
+ *Return: c
+ */
+
+char *cap_string(char *c)
+{
+	int i, j;
+	char ch[] = {' ', ',', ';', '\n', '\t', '.',
+		     '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; ch[j] != '\0'; j++)
+		{
+			if (c[i] == ch[j])
+			{
+				if (c[i + 1] >= 97 && c[i + 1] <= 122)
+				{
+					c[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (c);
+}
