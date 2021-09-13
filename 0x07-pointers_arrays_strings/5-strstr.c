ccedfd8f8d6bd5d23133b28065a85f1004f6ad88
der-bew <derbew12@gmail.com>
2021-09-13 23:03:08 +0300

    Add: 5-strstr.c file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,17 +8,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	unsigned int i = 0, j = 0;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	while (haystack[i])
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			if (haystack[i + j] != needle[j])
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
 				break;
 		}
-		if (!needle[j])
-			return (&haystack[i]);
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
-	return (NULL);
+	return (0);
 }
