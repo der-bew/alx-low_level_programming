fad7ca9a655556fe4b81339aff9c92dba628fa97
der-bew <derbew12@gmail.com>
2021-09-13 22:59:25 +0300

    Add: 5-strstr.c file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ **_strstr - Locates a substring
+ *@haystack: Input string
+ *@needle: Substring
+ *Return: needle or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
+}
