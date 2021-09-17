7cb102a4b15a9da290f363eabf2f24335048b365
der-bew <derbew12@gmail.com>
2021-09-17 16:33:07 +0300

    Add: 2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - Prints all arguments it receives
+ *@argc: Argument counter
+ *@argv: Argumnet array
+ *Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
