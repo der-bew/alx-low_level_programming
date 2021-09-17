47ea7ecdc28211b0c2e28cc3dcca30c4038301a6
der-bew <derbew12@gmail.com>
2021-09-17 15:51:33 +0300

    Fix the Warning

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  *Return: Always (0)
  */
 
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
-	printf("program name: %s \n", argv[0]);
+	printf("%s \n", argv[0]);
 
 	return (0);
 }
