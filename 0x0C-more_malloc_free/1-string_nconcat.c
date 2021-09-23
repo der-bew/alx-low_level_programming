9ecaee5d743f4f1849e011ca187e2381a8f21286
der-bew <derbew12@gmail.com>
2021-09-23 23:30:40 +0300

    Fix check-8 task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,35 +17,32 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i, j, k;
+	char *s;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-
-	if (n < len2)
-		ptr = malloc(sizeof(char) * (len1 + n + 1));
+	if (s1 == NULL)
+		i = 0;
 	else
-		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (!ptr)
-		return (NULL);
-
-	while (i < len1)
 	{
-		ptr[i] = s1[i];
-		i++;
+		for (i = 0; s1[i]; i++)
+			;
 	}
-
-	while (n < len2 && i < (len1 + n))
-		ptr[i++] = s2[j++];
-
-	while (n >= len2 && i < (len1 + len2))
-		ptr[i++] = s2[j++];
-
-	ptr[i] = '\0';
-
-	return (ptr);
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
 }
