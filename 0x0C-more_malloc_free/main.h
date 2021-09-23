87f234da523045e26f3e4ef16596a5ad2adf0d49
der-bew <derbew12@gmail.com>
2021-09-24 00:47:53 +0300

    Update: Header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,5 @@
-/* header files */
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -6,3 +7,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
