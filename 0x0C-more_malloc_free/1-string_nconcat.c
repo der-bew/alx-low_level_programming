a0fe509f2a8505f7e252ddcf5aaef6ced4e685e7
der-bew <derbew12@gmail.com>
2021-09-24 16:34:47 +0300

    Update: 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	i = 0;
 	while (s1[i] != '\0')
 		i++;
-	ptr = malloc((sizeof(*ptr) * i) + (sizeof(*ptr) * n) + 1);
+	ptr = malloc(sizeof(*ptr) * (i + n + 2));
 	if (ptr == NULL)
 		return (NULL);
 
