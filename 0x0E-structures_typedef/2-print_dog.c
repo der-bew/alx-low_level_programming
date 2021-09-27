ff2e11d30a0ab27d6581cd0a7b69f36c6d0fb69b
der-bew <derbew12@gmail.com>
2021-09-28 00:44:20 +0300

    Add: 2-print_dog.c file and Update: dog.h header file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *print_dog - Prints a struct dog
+ *@d: Input struct
+ *Return: Void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		printf("Name: ");
+		(*d).name == NULL ? printf("(nil)\n") : printf("%s\n", (*d).name);
+		printf("Age: %f\n", (*d).age);
+		printf("Owner: ");
+		(*d).owner == NULL ? printf("(nil)\n") : printf("%s\n", (*d).owner);
+	}
+}
