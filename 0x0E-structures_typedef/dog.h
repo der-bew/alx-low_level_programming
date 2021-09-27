9e518ed044a2055cbe25feaf6b0dc3cdcd8ce510
der-bew <derbew12@gmail.com>
2021-09-28 01:07:09 +0300

    Add: 4-new_dog.c file and Update: dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,8 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
-/*void free_dog(dog_t *d);*/
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* MYSTRUCT_H */
