1052e83bf48693bf93099bb4b4cde744c09b65ed
der-bew <derbew12@gmail.com>
2021-09-28 00:18:10 +0300

    Add: 1-init_dog.c file and Update: dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,15 @@
  *@age: Age of the dog
  *@owner: Owner of the dog
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 
 #endif /* MYSTRUCT_H */
