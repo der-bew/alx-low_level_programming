cf1e1f83235b0155986a866f4e3e7c274a484ce3
der-bew <derbew12@gmail.com>
2021-09-27 23:55:37 +0300

    Add: dog.h header file and README.md file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MYSTRUCT_H
+#define MYSTRUCT_H
+/**
+ *struct dog - creating a dog struct
+ *@name: Name of the dog
+ *@age: Age of the dog
+ *@owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* MYSTRUCT_H */
