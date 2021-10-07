4c81c4e3ef40965c74a1c745908fd892c1d464fb
der-bew <derbew12@gmail.com>
2021-10-08 00:38:07 +0300

    Update: 0-print_list.c file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,21 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t x = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 		h = h->next;
-		x++;
+		s++;
 	}
 
-	return (x);
+	return (s);
 }
