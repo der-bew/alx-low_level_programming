5c0a9efd3fa58902fe9eabdd3e2bd80a5a711fbe
der-bew <derbew12@gmail.com>
2021-11-25 15:38:48 +0300

    Update: 0-print_dlistint.c file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,9 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t l = 0;
 
-	if (h == NULL)
-		return (0);
-
 	while (h != NULL)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		l++;
 	}
