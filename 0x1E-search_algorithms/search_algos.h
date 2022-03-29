d117fe67f0b6d95e51d34566dc0e71fb7740768c
der-bew <derbew12@gmail.com>
2022-03-29 18:24:49 +0300

    My first commit. README.md and search_algos.h files added

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SEARCHALG_H_
+#define _SEARCHALG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif /*_SEARCHALG_H_ */
