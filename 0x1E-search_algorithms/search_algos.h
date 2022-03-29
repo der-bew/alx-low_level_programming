c4cc2f790f34dbcb4bf80fcbec72eed4cea993db
der-bew <derbew12@gmail.com>
2022-03-29 18:50:43 +0300

    Add: 1-binary.c file and Update: search_algos.h file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+void print_array(int *array, size_t size);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
